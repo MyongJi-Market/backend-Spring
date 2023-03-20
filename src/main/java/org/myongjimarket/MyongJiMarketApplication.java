13aa583b8fc0e75b62db8757083b5a97c713ea20
thingk0 <audtjd9080@naver.com>
2023-03-20 22:10:16 +0900

    MyongJi-Market Backend Prj:)

---
+++ src/main/java/org/myongjimarket/MyongJiMarketApplication.java
@@ -0,0 +1,13 @@
+package org.myongjimarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyongJiMarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyongJiMarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/myongjimarket/MyongJiMarketApplicationTests.java
@@ -0,0 +1,14 @@
+package org.myongjimarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootTest
+class MyongJiMarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
