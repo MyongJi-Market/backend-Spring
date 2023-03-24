2e862a1a6752d3c9185e8d5695eb8329cd54af07
thingk0 <audtjd9080@naver.com>
2023-03-24 17:47:13 +0900

    :sparkles: feat: MemberRepository 이메일을 통한 조회 기능.

--- src/main/java/org/myongjimarket/repository/MemberRepository.java
+++ src/main/java/org/myongjimarket/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package org.myongjimarket.repository;
 import org.myongjimarket.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
